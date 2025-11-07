95bf88a9d78ef27a94ab85dde48f38b136824fb5
Elvis Pichou <elvis.pichou@gmail.com>
2025-11-07 09:06:40 +0100

    📝 Docs(swagger): Update Swagger documentation for monsters and spells

--- src/resources/monsters/dtos/find-all.dto.ts
+++ src/resources/monsters/dtos/find-all.dto.ts
@@ -1,11 +1,24 @@
 import { Pagination } from "@/common/dtos/pagination.dto";
+import { ApiProperty } from "@nestjs/swagger";
 import { IsOptional, IsString } from "class-validator";
 
 export class PaginationMonster extends Pagination {
+  @ApiProperty({
+    description: "Filter monsters by name",
+    required: false,
+    example: "Goblin",
+    type: String,
+  })
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiProperty({
+    description: "Filter by language code (ISO 2 code)",
+    required: false,
+    example: "en",
+    type: String,
+  })
   @IsOptional()
   @IsString()
   lang?: string;
--- src/resources/spells/dtos/find-all.dto.ts
+++ src/resources/spells/dtos/find-all.dto.ts
@@ -1,13 +1,25 @@
 import { Pagination } from "@/common/dtos/pagination.dto";
+import { ApiProperty } from "@nestjs/swagger";
 import { IsOptional, IsString } from "class-validator";
 
 export class PaginationSpell extends Pagination {
+  @ApiProperty({
+    description: "Filter spells by name",
+    required: false,
+    example: "Fireball",
+    type: String,
+  })
+  @IsOptional()
+  @IsString()
+  name?: string;
 
-    @IsOptional()
-    @IsString()
-    name?: string
-
-    @IsOptional()
-    @IsString()
-    lang?: string
+  @ApiProperty({
+    description: "Filter by language code (ISO 2 code)",
+    required: false,
+    example: "en",
+    type: String,
+  })
+  @IsOptional()
+  @IsString()
+  lang?: string;
 }
